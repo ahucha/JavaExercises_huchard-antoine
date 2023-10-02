1a7ccc3f96432a22b7e3040ccc0e9cbbd3d6f345
ahucha <99647284+ahucha@users.noreply.github.com>
2023-10-02 10:09:00 +0200

    Add files via upload

---
+++ Exos-Java/exo1/exo1.java
@@ -0,0 +1,11 @@
+package exo1;
+
+public class exo1 {
+    public static void main(String[] args) {
+
+        String name = "Alex";
+        int age = 25;
+
+        System.out.println(name + age);
+    }
+}
