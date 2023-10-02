da0af2dfdef822131c22bb73932103121347d774
ahucha <99647284+ahucha@users.noreply.github.com>
2023-10-02 11:16:06 +0200

    Add files via upload

---
+++ exo2/exo2.java
@@ -0,0 +1,14 @@
+package exo2;
+
+public class exo2 {
+    public static void main(String[] args) {
+
+        double price = 19.99;
+        Integer quantity = 12;
+
+        double total = price * quantity;
+
+        System.out.println(total);
+    }
+
+}
